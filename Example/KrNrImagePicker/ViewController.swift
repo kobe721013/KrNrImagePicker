68ebe791bbe55eef8a8c439e8d6f38a17416258d
KobeLin <kobebryant721013@gmail.com>
2021-06-17 15:41:50 +0800

    [Add]         *Default code

--- Example/KrNrImagePicker/ViewController.swift
+++ Example/KrNrImagePicker/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
-
+import KrNrImagePicker
 class ViewController: UIViewController {
 
+    private var krnrSlideView:KrNrSlideView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +21,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    override func viewWillLayoutSubviews() {
+        
+        print("(ViewController)-viewWillLayoutSubviews: current bound=\(view.bounds)")
+        krnrSlideView.updateFrame(bounds: view.bounds)
+    }
+    
+    private func setUpUI() {
+        self.view.backgroundColor = #colorLiteral(red: 0.1725490196, green: 0.2431372549, blue: 0.3137254902, alpha: 1)
+        
+        krnrSlideView = KrNrSlideView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        self.view.addSubview(krnrSlideView)
+       
+        //bannerView.backgroundColor = UIColor.green
+       // krnrSlideView.reloadData()
+    }
+    
 
 }
 
